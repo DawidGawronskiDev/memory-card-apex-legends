83da1e6f8444375d1353033366ab33511eecddcf
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2023-10-13 21:51:04 +0100

    Create CardBack Component and refactor CardFront

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import Tilt from "react-parallax-tilt";
 import cardBackground from "/images/card-background.png";
+import Logo from "/images/logo.png";
 
 import "../styles/Card.css";
 
@@ -16,21 +17,43 @@ export default function Card({ character }) {
   return (
     <div className="card-wrapper" onClick={handleClick}>
       <Tilt className="card" flipHorizontally={isFlipped}>
-        <div
-          className="card-background"
-          style={{
-            backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.30) 100%), url(${cardBackground})`,
-          }}
-        />
-        <div
-          className="card-character"
-          style={{
-            backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #000 86.46%, #000 100%), url(${character.image})`,
-            mixBlendMode: "overlay",
-          }}
-        />
-        <span className="character-name">{character.name}</span>
+        {!isFlipped ? <CardFront character={character} /> : <CardBack />}
       </Tilt>
     </div>
+  );
+}
+
+function CardFront({ character }) {
+  return (
+    <>
+      <div
+        className="card-background"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.30) 100%), url(${cardBackground})`,
+        }}
+      />
+      <div
+        className="card-character"
+        style={{
+          backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #000 86.46%, #000 100%), url(${character.image})`,
+          mixBlendMode: "overlay",
+        }}
+      />
+      <span className="character-name">{character.name}</span>
+    </>
+  );
+}
+
+function CardBack() {
+  return (
+    <>
+      <div
+        className="card-background"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.50) 100%), url(${cardBackground})`,
+        }}
+      />
+      <img src={Logo} alt="" />
+    </>
   );
 }
