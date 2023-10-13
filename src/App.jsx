216cb84c1c1297d0ccce516d6479910ab8226620
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2023-10-13 16:32:22 +0100

    Prototype card list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+
+import Card from "./components/Card";
+
 function App() {
-  return <></>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetch("/data.json").then((resp) => resp.json());
+        setData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul className="card-list">
+      {data &&
+        data.characters.map((character) => (
+          <Card key={character.name} character={character} />
+        ))}
+    </ul>
+  );
 }
 
 export default App;
