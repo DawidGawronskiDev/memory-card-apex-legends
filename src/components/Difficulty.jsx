944447acf1c7d92064c579149290c818f6f72b32
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2023-10-15 12:35:44 +0100

    Add card background to Difficulty compoennt

--- src/components/Difficulty.jsx
+++ src/components/Difficulty.jsx
@@ -1,4 +1,8 @@
 /* eslint-disable react/prop-types */
+import CardBackground from "/images/card-background.png";
+
+import "../styles/Difficulty.css";
+
 export default function Difficulty() {
   return (
     <div className="difficulty">
@@ -12,7 +16,12 @@ export default function Difficulty() {
 function DifficultyContainer({ difficulty }) {
   return (
     <div className="difficulty-container">
-      <img src={`/images/${difficulty}-image.png`} alt="" />
+      <img
+        className="card-character"
+        src={`/images/${difficulty}-image.png`}
+        alt=""
+      />
+      <img className="card-background" src={CardBackground} alt="" />
       <span>{difficulty}</span>
     </div>
   );
